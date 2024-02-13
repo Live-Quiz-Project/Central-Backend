cc252bcccb2eb055c5a167d80c2ffbb01aa53fba
Phurit Sararattanakul <phurits@Phurits-MacBook-Air.local>
2024-02-13 11:47:04 +0700

    Add Unit Test in Quiz

--- internal/quiz/v1/repository_test.go
+++ internal/quiz/v1/repository_test.go
@@ -2,79 +2,170 @@ package v1
 
 import (
 	"context"
+	"database/sql"
 	"testing"
-	"time"
 
-	u "github.com/Live-Quiz-Project/Backend/internal/user/v1"
+	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
-func TestCreateUser(t *testing.T) {
-	// Create repository instance with the test database
-	repo := NewRepository(testDB)
-	user_repo := u.NewRepository(testDB)
-
-	tx := testDB.Begin()
-	defer tx.Rollback()
-
-	// Create a user to be inserted
-	user := &u.User{
-		ID:            uuid.New(),
-		Name:          "QuizTest User",
-		Email:         "quiz-user@tester.com",
-		Password:      "hashed_password",
-		Image:         "profile.jpg",
-		DisplayName:   "Quiz Test",
-		DisplayEmoji:  ":smile:",
-		DisplayColor:  "#3498db",
-		AccountStatus: "active",
-		CreatedAt:     time.Now(),
-		UpdatedAt:     time.Now(),
+func DbMock(t *testing.T) (*sql.DB, *gorm.DB, sqlmock.Sqlmock) {
+	sqldb, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatal(err)
 	}
+	gormdb, err := gorm.Open(postgres.New(postgres.Config{
+		Conn: sqldb,
+	}), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
 
-	createdUser, err := user_repo.CreateUser(context.Background(), user)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return sqldb, gormdb, mock
+}
 
-	assert.NoError(t, err)
+func TestCreateQuiz(t *testing.T) {
+	// Test Setup
+	sqlDB, db, mock := DbMock(t)
+	defer sqlDB.Close()
+	r := NewRepository(db)
 
-	assert.NotNil(t, createdUser)
+	// Variables
 
-	assert.Equal(t, user.Name, createdUser.Name)
-	assert.Equal(t, user.Email, createdUser.Email)
-	assert.Equal(t, user.Image, createdUser.Image)
-	assert.Equal(t, user.DisplayName, createdUser.DisplayName)
-	assert.Equal(t, user.DisplayEmoji, createdUser.DisplayEmoji)
-	assert.Equal(t, user.DisplayColor, createdUser.DisplayColor)
-
+	// Mock Data
 	quiz := &Quiz{
 		ID:             uuid.New(),
-		CreatorID:      user.ID,
-		Title:          "Test Create Quiz",
-		Description:    "TestCreateQuiz Function",
-		CoverImage:     "cover_image.jpg",
-		Visibility:     "public",
-		TimeLimit:      60,
-		HaveTimeFactor: true,
-		TimeFactor:     2,
-		FontSize:       16,
+		CreatorID:      uuid.New(),
+		Title:          "Test Title",
+		Description:    "Test Description",
+		CoverImage:     "Test CoverImage",
+		Visibility:     "PRIVATE",
+		TimeLimit:      20,
+		HaveTimeFactor: false,
+		TimeFactor:     1,
+		FontSize:       24,
 		Mark:           10,
 		SelectMin:      1,
-		SelectMax:      4,
-		CaseSensitive:  false,
-		CreatedAt:      time.Now(),
-		UpdatedAt:      time.Now(),
+		SelectMax:      1,
+		CaseSensitive:  true,
 	}
 
-	createdQuiz, err := repo.CreateQuiz(context.Background(), tx, quiz)
-	tx.Commit()
+	// Add rows in 'Test' Database
 
-	// You can also retrieve the user from the database to make sure it was actually inserted
-	retrievedQuiz, err := repo.GetQuizByID(context.Background(), quiz.ID)
+	// Expected Query
+	expectedSQL := "INSERT INTO \"quiz\" (.+) VALUES (.+)"
+	mock.ExpectBegin()
+	mock.ExpectExec(expectedSQL).
+		WithArgs(quiz.ID.String(),quiz.CreatorID.String(), quiz.Title, quiz.Description, quiz.CoverImage, quiz.Visibility, quiz.TimeLimit, quiz.HaveTimeFactor, quiz.TimeFactor, quiz.FontSize, quiz.Mark, quiz.SelectMin, quiz.SelectMax, quiz.CaseSensitive,sqlmock.AnyArg(),sqlmock.AnyArg(),sqlmock.AnyArg()).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectCommit()
 
-	assert.Equal(t, createdQuiz.ID, retrievedQuiz.ID)
-	assert.Equal(t, createdQuiz.CreatorID, retrievedQuiz.CreatorID)
-	assert.Equal(t, createdQuiz.Title, retrievedQuiz.Title)
-	assert.Equal(t, createdQuiz.Description, retrievedQuiz.Description)
-	assert.Equal(t, createdQuiz.CoverImage, retrievedQuiz.CoverImage)
-	assert.Equal(t, createdQuiz.Visibility, retrievedQuiz.Visibility)
+	// Actual Function
+	res, err := r.CreateQuiz(context.TODO(), db, quiz)
+
+	// Unit Test
+	assert.Nil(t, err)
+	assert.NotNil(t, res)
+	assert.Nil(t, mock.ExpectationsWereMet())
 }
+
+func TestGetQuizzesByUserID(t *testing.T) {
+	// Test Setup
+	sqlDB, db, mock := DbMock(t)
+	defer sqlDB.Close()
+	repo := NewRepository(db)
+
+	// Variables
+	id := uuid.New()
+
+	// Mock Data
+	quiz := &Quiz{
+		ID:             uuid.New(),
+		CreatorID:      id,
+		Title:          "Test Title",
+		Description:    "Test Description",
+		CoverImage:     "Test CoverImage",
+		Visibility:     "PRIVATE",
+		TimeLimit:      20,
+		HaveTimeFactor: false,
+		TimeFactor:     1,
+		FontSize:       24,
+		Mark:           10,
+		SelectMin:      1,
+		SelectMax:      1,
+		CaseSensitive:  true,
+	}
+	
+	// Add rows in 'Test' database
+	quizzes := sqlmock.NewRows([]string{"id", "creator_id", "title", "description", "cover_image", "visibility", "time_limit", "have_time_factor", "time_factor", "font_size", "mark", "select_min", "select_max", "case_sensitive"}).
+		AddRow(quiz.ID.String(),quiz.CreatorID.String(), quiz.Title, quiz.Description, quiz.CoverImage, quiz.Visibility, quiz.TimeLimit, quiz.HaveTimeFactor, quiz.TimeFactor, quiz.FontSize, quiz.Mark, quiz.SelectMin, quiz.SelectMax, quiz.CaseSensitive)
+
+	// Expected Query
+	expectedSQL := "SELECT (.+) FROM \"quiz\" WHERE creator_id =(.+)"
+	mock.ExpectQuery(expectedSQL).
+		WithArgs(id.String()).
+		WillReturnRows(quizzes)
+
+	// Actual Function
+	res, err := repo.GetQuizzesByUserID(context.TODO(), id)
+
+	// Unit Test
+	assert.Nil(t, err)
+	assert.NotNil(t, res)
+	assert.Nil(t, mock.ExpectationsWereMet())	
+}
+
+
+func TestGetQuizByID(t *testing.T) {
+	// Setup Test
+	sqlDB, db, mock := DbMock(t)
+	defer sqlDB.Close()
+	repo := NewRepository(db)
+
+	// Variables
+	id := uuid.New()
+
+	// Mock Data
+	quiz := &Quiz{
+		ID:             id,
+		CreatorID:      uuid.New(),
+		Title:          "Test Title",
+		Description:    "Test Description",
+		CoverImage:     "Test CoverImage",
+		Visibility:     "PRIVATE",
+		TimeLimit:      20,
+		HaveTimeFactor: false,
+		TimeFactor:     1,
+		FontSize:       24,
+		Mark:           10,
+		SelectMin:      1,
+		SelectMax:      1,
+		CaseSensitive:  true,
+	}
+
+	// Add rows to 'Test' Database
+	quizzes := sqlmock.NewRows([]string{"id", "creator_id", "title", "description", "cover_image", "visibility", "time_limit", "have_time_factor", "time_factor", "font_size", "mark", "select_min", "select_max", "case_sensitive"}).
+		AddRow(quiz.ID.String(),quiz.CreatorID.String(), quiz.Title, quiz.Description, quiz.CoverImage, quiz.Visibility, quiz.TimeLimit, quiz.HaveTimeFactor, quiz.TimeFactor, quiz.FontSize, quiz.Mark, quiz.SelectMin, quiz.SelectMax, quiz.CaseSensitive)
+
+	// Expected Query
+	expectedSQL := "SELECT (.+) FROM \"quiz\" WHERE id =(.+)"
+	mock.ExpectQuery(expectedSQL).
+		WithArgs(id.String()).
+		WillReturnRows(quizzes)
+
+	// Actual Function
+	res, err := repo.GetQuizByID(context.TODO(), id)
+
+	// Unit Test
+	assert.Nil(t, err)
+	assert.NotNil(t, res)
+	assert.Nil(t, mock.ExpectationsWereMet())
+}
+
+
